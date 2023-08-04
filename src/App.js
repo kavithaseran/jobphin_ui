60de54477ff43a720e290a9c0dfafdb376142d42
kavithaseran <kavithaseran@gmail.com>
2023-08-04 23:53:19 +0530

    crud operations for jobs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import appRoutes from './app/router';
+import Loader from './components/Loader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-2.5">
+      <BrowserRouter>
+          <Suspense fallback={Loader()}>
+              <Routes>
+                {appRoutes.map((route) => (
+                  <Route path={route.path} element={route.element} key={route.path}/>
+                ))}
+              </Routes>
+          </Suspense>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Popup.js
@@ -0,0 +1,31 @@
+import Button from "./Button"
+import { NO, OKAY, YES } from "../constants";
+
+const Popup = (props) => {
+    const {confirmationText, yesHandler, noHandler, isInfo} = props;
+    return (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-10" id="popup-modal">
+        <div className="relative top-20 m-auto p-8 border min-h-max max-w-xl min-w-96 shadow-lg rounded-md bg-white">
+            <p className="mb-12">{confirmationText}</p>
+            {!isInfo && <div className="flex gap-5">
+                <Button 
+                    buttonText = {YES}
+                    onClickHandler={yesHandler}
+                />
+                <Button 
+                    buttonText = {NO}
+                    onClickHandler={noHandler}
+                />
+            </div>}
+            {isInfo && 
+                <Button 
+                    buttonText = {OKAY}
+                    onClickHandler={yesHandler}
+                />
+            }
+        </div>
+    </div>
+    )
+}
+
+export default Popup;
